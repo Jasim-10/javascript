e09a2e456461b37ed14f1419e478482b0ad73288
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-09-26 13:36:17 +0530

    basic

---
+++ Basic/01_variables.js
@@ -0,0 +1,13 @@
+// console.log("Hello World! ")
+
+
+
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+console.log(accountId);
+console.table([accountId, accountEmail,accountPass,accountCity,accountState])
---
+++ Basic/02_dataTypes.js
@@ -0,0 +1,4 @@
+"use strict";          //treat all JS code as newer Version
+
+// alert(3+6)      //we are using nodejs,not browser
+
