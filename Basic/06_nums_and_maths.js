a147233d518ec7190e88b4033b18607ffba9b3cc
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-09-28 17:30:25 +0530

    basic

--- Basic/06_nums_and_maths.js
+++ Basic/06_nums_and_maths.js
@@ -16,3 +16,26 @@
 
 
 
+// ******************Maths********************
+// console.log(Math.PI)
+// console.log(Math.abs(-5))
+// console.log(Math.round(4.2))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(4,8,2,9,0,7))
+// console.log(Math.max(7,8,2,6,11,66,24,20))
+
+
+// console.log(Math.random())
+// console.log(Math.random()*10)
+// console.log((Math.random()*10)+1)
+// console.log(Math.floor(Math.random()*10)+1)
+
+// const min = 25
+// const max = 35
+// console.log(Math.floor(Math.random()*(max-min + 1))+min)
+
+
+
+
+
---
+++ Basic/07_dates.js
@@ -0,0 +1,27 @@
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON)
+// console.log(typeof(myDate))
+
+
+// let myCreateDate = new Date(2023,0,23)          //months start with 0 in js
+// console.log(myCreateDate.toLocaleString()) 
+// console.log(myCreateDate.toDateString()) 
+
+
+// let myCreateDate = new Date(2023,0,23)
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(myCreateDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+
+
+// let date = new Date()
+// console.log(date.getDate())
+// console.log(date.getMonth()+1)
+// console.log(date.getSeconds())
+
