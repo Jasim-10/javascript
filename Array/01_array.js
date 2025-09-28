9c1d2455b7eb14aa4661ca706c9a1eee2599f060
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-09-28 17:30:12 +0530

    Array

---
+++ Array/01_array.js
@@ -0,0 +1,76 @@
+// const myArr = [0,1,2,3,4,5,true,"jasim"]
+// // console.log(myArr)
+// // console.log(myArr[2])
+// console.log(myArr[6])
+// console.log(myArr[7])
+// console.log(myArr[-1])      //undefined
+
+
+
+
+// const myArr = [1,2,3,4,5]
+// myArr.push(6)
+// console.log(myArr)
+// console.log(myArr.length)
+// myArr.pop()
+// console.log(myArr)
+// console.log(myArr.length)
+// myArr.unshift(9)
+// console.log(myArr)
+// myArr.shift()
+// console.log(myArr)
+
+
+
+
+// const myArr = [0,1,2,3,4,5,true,"jasim"]
+// console.log(myArr.includes(4))
+// console.log(myArr.indexOf(true))
+
+
+// const myArr = [1,2,3,4]
+// const newArr = myArr.join()
+// console.log(newArr)
+// console.log(typeof(newArr))
+
+
+// const myArr = [1,2,3,4,5,6,7]
+// console.log(myArr.slice(1,4))
+// console.log("my arr after slice() ",myArr)
+// console.log(myArr.splice(1,4))
+// console.log("my arr after splice() ",myArr)
+
+
+
+
+// const marvel_heroes = ["ironman","captain america","thor"]
+// const dc_heroes = ["superman","batman","wonder woman"]
+// marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes)
+
+
+// const marvel_heroes = ["ironman","captain america","thor"]
+// const dc_heroes = ["superman","batman","wonder woman"]
+// const all_heroes = marvel_heroes.concat(dc_heroes)
+// console.log(all_heroes)
+
+
+// const marvel_heroes = ["ironman","captain america","thor"]
+// const dc_heroes = ["superman","batman","wonder woman"]
+// const all_heroes = [...marvel_heroes, ...dc_heroes]           //spread operator
+// console.log(all_heroes)
+
+
+// const arr = [1,2,3,[4,5,6],7,[4,2,5,[5,2,5]]]
+// const useable_arr = arr.flat(Infinity)
+// console.log(useable_arr)
+
+
+
+// console.log(Array.isArray("Jasim"))
+// console.log(Array.from("Jasim"))
+// console.log(Array.from({name: "jasim"}))            //return empty array
+
+
+
+
