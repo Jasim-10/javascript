29371a5b4cf5d519fcfa150fa528a60791315ab0
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-10-03 17:04:03 +0530

    object

--- Array/object.js
+++ Array/object.js
@@ -86,8 +86,19 @@
 
 
 
-const obj1 = {1:"a",2:"b"}
-const obj2 = {2:"c",3:"d"}
-// const obj3 = Object.assign({},obj1,obj2)
-const obj3 = {...obj1, ...obj2}
-console.log(obj3)
+// const obj1 = {1:"a",2:"b"}
+// const obj2 = {2:"c",3:"d"}
+// // const obj3 = Object.assign({},obj1,obj2)
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3)
+
+
+
+
+// const course = {
+//     coursename: "js in hindi",
+//     price: "999",
+//     courseInstructor: "Ravdeep"
+// }
+// const {courseInstructor:instructor} = course
+// console.log(instructor)
