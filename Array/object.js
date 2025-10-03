5a8584ea4eaede030d83549aad84523eb45ec128
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-10-03 13:03:36 +0530

    object

---
+++ Array/object.js
@@ -0,0 +1,74 @@
+//object literals
+
+// const JsUser = {
+//     name: "Jasim",
+//     age: 18,
+//     location: "Jaipur",
+//     email: "jasim@gmail.com",
+//     isLogged: false,
+//     lastLogin: ["Monday", "Saturday"]
+// };
+
+// console.log(JsUser.age);
+// console.log(JsUser["email"])
+// console.log(JsUser["age"])
+
+
+
+
+// const mySym = Symbol("key1")
+
+// const JsUser = {
+//     name: "Jasim",
+//     age: 18,
+//     [mySym]: "mykey1",
+//     location: "Jaipur",
+//     email: "jasim@gmail.com",
+//     isLogged: false,
+//     lastLogin: ["Monday", "Saturday"]
+// };
+
+// console.log(JsUser.age);
+// console.log(JsUser["email"])
+// console.log(JsUser["age"])
+// console.log(JsUser[mySym])
+
+// JsUser.email = "jasim@chatgpt.com"
+// Object.freeze(JsUser)                        //after that we cannot change anything in object
+// JsUser.age = 25
+// console.log(JsUser)
+
+
+
+
+// const JsUser = {
+//     name: "Jasim",
+//     age: 18,
+//     location: "Jaipur",
+//     email: "jasim@gmail.com",
+//     isLogged: false,
+//     lastLogin: ["Monday", "Saturday"]
+// };
+// JsUser.greeting = function(){
+//     console.log("Hello Js User")
+//     console.log(`Hello Js User , ${this.name}`)
+// }
+// console.log(JsUser.greeting())
+
+
+
+
+
+
+//singleton
+
+// const tinderUser = new Object()
+// console.log(tinderUser)
+
+
+// const tinderUser = {}
+// tinderUser.id = 123
+// tinderUser.name = "Sam"
+// tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
